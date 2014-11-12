51cc4bc616e958cc7a2e17c7dbd3271d3832605c
ViksUI <frode@aktivportal.com>
2014-11-12 03:19:34 +0100

    Added Zygor skin

---
+++ ViksUI/Modules/Skins/zygor.lua
@@ -0,0 +1,17 @@
+﻿local T, Viks, L, _ = unpack(select(2, ...))
+if Viks.skins.zygor ~= true then return end
+
+----------------------------------------------------------------------------------------
+--	Zygor skin
+----------------------------------------------------------------------------------------
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event)
+	if not IsAddOnLoaded("ZygorGuidesViewer") then return end
+	ZygorGuidesViewerFrame:StripTextures(true)
+	ZygorGuidesViewerFrame_Border:SetTemplate("Transparent")
+	ZygorGuidesViewer_CreatureViewer:StripTextures(true)
+	ZygorGuidesViewer_CreatureViewer:SetTemplate("Transparent", true)
+
+
+end)
